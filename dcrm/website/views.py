c4e320f5b0b67ac5a1a7cbf48b41bf1b5fb2a182
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-07-18 23:16:34 -0600

    se agrega un login

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
+    # verifica usuario
+    if request.method == 'POST':
+        usuario = request.POST['username']
+        contrasena = request.POST['password']
+        # autenticar
+        user = authenticate(request,
+                            username=usuario,
+                            password=contrasena)
+        
+        if user is not None:
+            login(request,user)
+            messages.success(request,'Bienvenido... ')
+            return redirect('home') 
+
     return render(request,'home.html',{})
+
+# def login_user(request):
+#     pass
+
+# def logout_user(request):
+#     pass
