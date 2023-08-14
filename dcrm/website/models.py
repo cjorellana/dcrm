1546f0e23d44a3b34d5db342078864f62c827b11
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-08-13 21:41:16 -0600

    se corrige error.

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Record(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    first_name = models.CharField(max_lengthx=100)
+    first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
